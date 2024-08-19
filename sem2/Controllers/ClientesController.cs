8e9886d2639b3ee64de971cb1353546e30f0693c
Pardalzovisk <enricodepace@hotmail.com>
2024-08-19 20:53:19 -0300

    a

--- sem2/Controllers/ClientesController.cs
+++ sem2/Controllers/ClientesController.cs
@@ -30,5 +30,44 @@ namespace sem2.Controllers
         {
             return View(Clientes);
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Cliente cliente)
+        {
+            cliente.ID = Clientes.Select(x => x.ID).Max() + 1;
+            Clientes.Add(cliente);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit( int id)
+        {
+            return View(Clientes.Where(cli => cli.ID == id).First());
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Cliente cliente)
+        {
+            var clienteOld = Clientes.Where(cli => cliente.ID == cliente.ID).First(); Clientes.Remove(clienteOld);Clientes.Add(cliente);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Details( int id)
+        {
+            return View(Clientes.Where(cli=>cli.ID==id).First());
+        }
+        public IActionResult Delete(int id)
+        {
+            return View(Clientes.Where(cli=>cli.ID == id).First());
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var cliente = Clientes.Where(cliente => cliente.ID == id).First();
+            Clientes.Remove(cliente);
+            return RedirectToAction("Index");
+        }
     }
 }
